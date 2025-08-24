065f55f49ca8f455f7d3b76f923d05b1fcf76067
Ramone Henry <ramoneh94@gmail.com>
2025-08-24 17:34:34 -0400

    adjust background container, allow main screen to be positioned on top of background shell correctly

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,7 +2,7 @@ import PlantCarousel from '@renderer/components/PlantCarousel'
 
 function App(): React.JSX.Element {
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
+    <div className="min-h-screen py-24">
       <PlantCarousel />
     </div>
   );
